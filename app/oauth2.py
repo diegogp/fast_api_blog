3a3b357ca13d6772a81f321c1493bec06dd6f0da
root <root@Diego.localdomain>
2022-08-16 17:09:27 -0300

    Protecting routes with login deps.

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,13 +1,19 @@
+from wsgiref import headers
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 #SECRET_KEY - Random long hex 32
 #Algorithm
 #Expiration time
 
 SECRET_KEY = "9532a2e4c01474cddc2ec650b4ca71681060c7dc2b3b6587cacbba910f12a703" #openssl rand -hex 32
 ALGORITHM = "HS256"
-ACESS_TOKEN_EXPIRE_MINUTES = 30
+ACESS_TOKEN_EXPIRE_MINUTES = 1
 
 def create_acess_token(data: dict):
     to_encode = data.copy()
@@ -17,3 +23,19 @@ def create_acess_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     
     return encoded_jwt
+
+def verify_acess_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could'nt validate credentials.",
+        headers={"WWW-Authenticate": "Bearer"})
+    return verify_acess_token(token, credentials_exception)
--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,5 @@
+from turtle import st
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
@@ -33,3 +35,10 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class Token(BaseModel):
+    acecss_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str]
